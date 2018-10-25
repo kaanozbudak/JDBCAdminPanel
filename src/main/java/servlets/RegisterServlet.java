69a5f774ca906ba2f3bca5da34bb000eba42bbdd
kaanozbudak <kaanozbudak@hotmail.com>
2018-10-25 20:45:20 +0300

    web xml fixed, nobody didnt declare servlet mapping, i did it !

--- src/main/java/servlets/RegisterServlet.java
+++ src/main/java/servlets/RegisterServlet.java
@@ -1,6 +1,5 @@
 package servlets;
-import handler.User;
-import handler.Database;
+import handler.*;
 
 import java.io.IOException;
 
@@ -9,11 +8,17 @@ public class RegisterServlet extends javax.servlet.http.HttpServlet {
 
         String url = "";
         Database database = new Database();
-        User user = new User(request.getParameter("firstname")
-                , request.getParameter("lastname")
-                , request.getParameter("username")
-                , request.getParameter("email")
-                , request.getParameter("password"));
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String userName = request.getParameter("userName");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        User user = new User(firstName,lastName,userName,email,password);
+        System.out.println(firstName + "/" + lastName + "/" + userName + "/" + email + "/" + password);
+
+
+
+
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
