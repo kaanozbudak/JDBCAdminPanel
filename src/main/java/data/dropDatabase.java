d3e3fc932272a08c5f7bfcfa20be0b536db9589d
Kubilay Çakmak <31804832+kubilayckmk@users.noreply.github.com>
2018-12-10 09:55:41 +0300

    Update dropDatabase.java

--- src/main/java/data/dropDatabase.java
+++ src/main/java/data/dropDatabase.java
@@ -7,15 +7,18 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class dropDatabase extends javax.servlet.http.HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws javax.servlet.ServletException, IOException, NullPointerException {
         Database database = new Database();
         String elementDbName = request.getParameter("databases");
+        HttpSession session = request.getSession();
         System.out.println(elementDbName);
-        request.setAttribute("elementDbName",elementDbName);
-
+        session.setAttribute("elementDbName",elementDbName);
+        ArrayList<String> arraylist = (ArrayList<String>) session.getAttribute("databases");
+        arraylist.remove(elementDbName);
         database.dropDatabase(elementDbName);
 
         request.getRequestDispatcher("showDatabases.jsp").forward(request,response);
